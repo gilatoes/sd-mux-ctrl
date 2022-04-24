d0f9da41e0eb5e890318b21b760b5d9e94d87e05
Gary <gilatoes@gmail.com>
2022-04-24 13:11:38 +0800

    initial commit

---
+++ build/src/CMakeFiles/sd-mux-ctrl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sd-mux-ctrl.dir/main.cpp.o"
+  "sd-mux-ctrl.pdb"
+  "sd-mux-ctrl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sd-mux-ctrl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
